5e8169c85eb96a90abbb34791743f5d744722973
vivekChenna <chennavivek96@gmail.com>
2024-12-17 13:28:14 +0530

    andaihub decision helping maker

---
+++ app/layout.tsx
@@ -0,0 +1,32 @@
+import type { Metadata } from "next";
+import { GeistSans } from "geist/font/sans";
+import { GeistMono } from "geist/font/mono";
+import "./globals.css";
+import { Nav } from "@/components/Nav";
+import { cn } from "@/utils";
+
+export const metadata: Metadata = {
+  title: "Andaihub Decision Helping Maker",
+  description: "Andaihub Decision Helping Maker",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+        className={cn(
+          GeistSans.variable,
+          GeistMono.variable,
+          "flex flex-col min-h-screen"
+        )}
+      >
+        <Nav />
+        {children}
+      </body>
+    </html>
+  );
+}
---
+++ components/Nav.tsx
@@ -0,0 +1,73 @@
+"use client";
+
+import { useLayoutEffect, useState } from "react";
+import HumeLogo from "./logos/Hume";
+import { Button } from "./ui/button";
+import { Moon, Sun } from "lucide-react";
+import Github from "./logos/GitHub";
+import pkg from '@/package.json';
+import Image from "next/image";
+
+export const Nav = () => {
+  const [isDarkMode, setIsDarkMode] = useState(false);
+
+  useLayoutEffect(() => {
+    const el = document.documentElement;
+
+    if (el.classList.contains("dark")) {
+      setIsDarkMode(true);
+    } else {
+      setIsDarkMode(false);
+    }
+  }, []);
+
+  const toggleDark = () => {
+    const el = document.documentElement;
+    el.classList.toggle("dark");
+    setIsDarkMode((prev) => !prev);
+  };
+
+  return (
+    <div
+      className={
+        "px-4 py-2 flex items-center h-14 z-50 bg-card border-b border-border"
+      }
+    >
+      <div>
+      <Image height={50} width={50} src={'/andai.png'} alt="Andaihub" className=' rounded' />
+      </div>
+      <div className={"ml-auto flex items-center gap-1"}>
+        {/* <Button
+          onClick={() => {
+            window.open(
+              pkg.homepage,
+              "_blank",
+              "noopener noreferrer"
+            );
+          }}
+          variant={"ghost"}
+          className={"ml-auto flex items-center gap-1.5"}
+        >
+          <span>
+            <Github className={"size-4"} />
+          </span>
+          <span>Star on GitHub</span>
+        </Button> */}
+        <Button
+          onClick={toggleDark}
+          variant={"ghost"}
+          className={"ml-auto flex items-center gap-1.5"}
+        >
+          <span>
+            {isDarkMode ? (
+              <Sun className={"size-4"} />
+            ) : (
+              <Moon className={"size-4"} />
+            )}
+          </span>
+          <span>{isDarkMode ? "Light" : "Dark"} Mode</span>
+        </Button>
+      </div>
+    </div>
+  );
+};
